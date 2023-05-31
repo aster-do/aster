bffce25227c0bcd6e621c9d92aea23f898efef42
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2023-05-31 15:25:08 +0200

    feat(common): add messaging factory trait
    Signed-off-by: Simon LUCIDO <simon.lucido@etu.umontpellier.fr>

--- common/src/messaging/mod.rs
+++ common/src/messaging/mod.rs
@@ -4,6 +4,8 @@ pub mod crossbeam;
 
 pub use crossbeam::{CrossbeamReceiver, CrossbeamSender};
 
+use self::crossbeam::{BillableReceiver, BillableSender, MetricReceiver, MetricSender};
+
 #[async_trait]
 pub trait AsyncReceiver<T, E: std::error::Error> {
     async fn receive(&mut self) -> Result<T, E>;
@@ -12,4 +14,12 @@ pub trait AsyncReceiver<T, E: std::error::Error> {
 #[async_trait]
 pub trait AsyncSender<T: Send, E: std::error::Error> {
     async fn send(&mut self, message: T) -> Result<(), E>;
+}
+
+#[async_trait]
+pub trait MessagingFactory {
+    async fn create_billable_sender(&self) -> BillableSender;
+    async fn create_billable_receiver(&self) -> BillableReceiver;
+    async fn create_metric_sender(&self) -> MetricSender;
+    async fn create_metric_receiver(&self) -> MetricReceiver;
 }
