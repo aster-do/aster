2fd402e275fcbb977ad6226288c68c1ad6a9db50
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2023-05-31 16:57:25 +0200

    feat: add init logic in aster
    Signed-off-by: Simon LUCIDO <simon.lucido@etu.umontpellier.fr>

--- aster/src/main.rs
+++ aster/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use common::AsterService;
+use log::info;
+
+#[tokio::main]
+async fn main() {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+
+    info!("Starting Aster");
+
+    let _messager = common::messaging::crossbeam::CrossbeamMessagingFactory::default();
+
+    // Create and init here
+    info!("Initializing services");
+    let mut services: Vec<Box<dyn AsterService>> = vec![];
+
+    let mut handles = vec![];
+
+    info!("Running services");
+    for service in services.iter_mut() {
+        handles.push(service.run());
+    }
+
+    futures::future::join_all(handles).await;
 }
