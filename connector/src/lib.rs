9f494ed718436b3e2fada97ea9d644bf98195e3d
iverly <github@iverly.net>
2023-05-31 18:49:47 +0200

    wip: ConnectorService
    Signed-off-by: iverly <github@iverly.net>

--- connector/src/lib.rs
+++ connector/src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use async_trait::async_trait;
+use common::{
+    messaging::{
+        crossbeam::{BillableReceiver, CrossbeamMessagingFactory},
+        MessagingFactory,
+    },
+    AsterService,
+};
+use sqlx::{postgres::PgPoolOptions, PgPool};
+
+#[derive(Clone, Default)]
+struct ConnectorService {
+    state: Option<ConnectorServiceState>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Clone)]
+struct ConnectorServiceState {
+    billable_receiver: BillableReceiver,
+    postgres_pool: PgPool,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[async_trait]
+impl AsterService for ConnectorService {
+    async fn init(&mut self, messaging: &mut CrossbeamMessagingFactory) {
+        // Connect to the database.
+        let postgres_pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect("postgres://postgres:postgres@localhost/postgres")
+            .await
+            .unwrap();
+
+        // Create the billable receiver.
+        self.state = Some(ConnectorServiceState {
+            billable_receiver: messaging.create_billable_receiver().await,
+            postgres_pool,
+        });
     }
+
+    async fn run(&mut self) {}
 }
