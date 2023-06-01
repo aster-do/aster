4cbbf3f89ed92d7f08d8980b222b1dab3940dc7f
iverly <github@iverly.net>
2023-06-01 13:47:44 +0200

    fix: add AsterServiceError for init/run functions
    Signed-off-by: iverly <github@iverly.net>

--- common/src/services/mod.rs
+++ common/src/services/mod.rs
@@ -1,9 +1,21 @@
 use async_trait::async_trait;
+use thiserror::Error;
 
 use crate::messaging::crossbeam::CrossbeamMessagingFactory;
 
+#[derive(Error, Debug)]
+pub enum AsterServiceError {
+    #[error("Failed to initialize Aster service: {0}")]
+    AsterServiceInitFailed(anyhow::Error),
+    #[error("Failed to run Aster service: {0}")]
+    AsterServiceRunFailed(anyhow::Error),
+}
+
 #[async_trait]
 pub trait AsterService {
-    async fn init(&mut self, messaging: &mut CrossbeamMessagingFactory);
-    async fn run(&mut self);
+    async fn init(
+        &mut self,
+        messaging: &mut CrossbeamMessagingFactory,
+    ) -> Result<(), AsterServiceError>;
+    async fn run(&mut self) -> Result<(), AsterServiceError>;
 }
