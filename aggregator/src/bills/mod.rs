6f80566ac34b9ee25e7b925d81ed5070872edb1c
WoodenMaiden <yann.pomie@laposte.net>
2023-06-01 15:49:49 +0200

    feat: aggregation and transaction handling
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

--- aggregator/src/bills/mod.rs
+++ aggregator/src/bills/mod.rs
@@ -11,8 +11,22 @@ pub mod billable_controller;
 #[derive(Clone, Debug, PartialEq, PartialOrd, sqlx::FromRow)]
 pub struct StoredBillable {
     pub id: Uuid,
+    /// name of the metric
     pub name: String,
+    /// Total price of bill
     pub price: i64,
+    /// Timestamp at which the bill was issued
     pub timestamp: DateTime<Utc>,
+    /// Value per unit of the metric
     pub value: f64,
+    /// Whether the bill has been treated or not
+    pub treated: bool,
+}
+
+pub struct _BillPerMetric {
+    pub id: Option<Uuid>,
+    /// name of the metric
+    pub name: String,
+    /// total price of bill
+    pub price: i64,
 }
