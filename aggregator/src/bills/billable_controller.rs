4faf05c80259b0102adf29a74a6b8ed4223763a5
WoodenMaiden <yann.pomie@laposte.net>
2023-06-01 15:49:49 +0200

    feat: aggregation and transaction handling
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

--- aggregator/src/bills/billable_controller.rs
+++ aggregator/src/bills/billable_controller.rs
@@ -1,7 +1,7 @@
-use crate::bills::StoredBillable;
+use crate::bills::{StoredBillable, _BillPerMetric as BillPerMetric};
 
-use log::{debug, info};
-use sqlx::{query_as, PgPool};
+use log::{debug, error, info};
+use sqlx::{query, query_as, types::uuid::Uuid, PgPool};
 use std::error::Error;
 
 pub struct _BillableController {
@@ -24,6 +24,37 @@ impl _BillableController {
         Ok(results)
     }
 
+    /// This will take metrics and aggregate them by name
+    fn _aggregate(billings: Vec<StoredBillable>) -> Vec<BillPerMetric> {
+        let mut bills_per_metric: Vec<BillPerMetric> = Vec::new();
+
+        billings.into_iter().for_each(|bill| {
+            let bill_per_metric = BillPerMetric {
+                id: None,
+                name: bill.name,
+                price: bill.price,
+            };
+
+            if bills_per_metric.is_empty() {
+                bills_per_metric.push(bill_per_metric);
+            } else {
+                let mut found = false;
+                for bill in bills_per_metric.iter_mut() {
+                    if bill.name == bill_per_metric.name {
+                        bill.price += bill_per_metric.price;
+                        found = true;
+                        break;
+                    }
+                }
+                if !found {
+                    bills_per_metric.push(bill_per_metric);
+                }
+            }
+        });
+
+        bills_per_metric
+    }
+
     pub async fn _run_aggregation_pipeline(&mut self) -> Result<(), Box<dyn Error>> {
         let billings = self._get_raw_billings().await?;
         debug!("Got {:?}", billings);
@@ -35,25 +66,75 @@ impl _BillableController {
 
         info!("Got {} billings from database", billings.len());
 
-        // let aggreged: AggregedBillable = billings.into_iter().fold(
-        //     AggregedBillable {},
-        //     |acc, bill| {
-        //         acc
-        //     }
-        // );
+        let ids = billings.iter().map(|b| b.id).collect::<Vec<Uuid>>();
 
-        // // Because we are never too careful
-        // let mut transaction = self.client.transaction().await?;
+        _BillableController::_aggregate(billings);
 
-        // futures_util::future::try_join(
-        //      transaction.query("INSERT INTO BILLABLE ...", &[]),
-        //      billings.into_iter().map(|bill| {
-        //          transaction.query("DELETE FROM BILLABLE WHERE ...", &[])
-        //      })
-        // );
+        // Because we are never too careful we start a transaction
+        let mut transaction = self.connection.begin().await?;
+
+        match futures_util::future::try_join(
+            query("UPDATE BILLABLE SET TREATED = FALSE WHERE ID = ANY($1)")
+                .bind(&ids[..])
+                .execute(&mut transaction),
+            futures_util::future::ok(()),
+        )
+        .await
+        {
+            Ok(_) => transaction.commit().await?,
+            Err(e) => {
+                error!("Error updating billables: {}", e);
+                transaction.rollback().await?;
+                return Err(e.into());
+            }
+        };
 
         info!("Aggregation pipeline ran sucessfully!");
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use uuid::Uuid;
+
+    #[test]
+    fn test_aggregation() {
+        let billings = vec![
+            StoredBillable {
+                id: Uuid::new_v4(),
+                name: "test".to_string(),
+                price: 10,
+                timestamp: chrono::Utc::now(),
+                value: 1.0,
+                treated: false,
+            },
+            StoredBillable {
+                id: Uuid::new_v4(),
+                name: "test".to_string(),
+                price: 10,
+                timestamp: chrono::Utc::now(),
+                value: 1.0,
+                treated: false,
+            },
+            StoredBillable {
+                id: Uuid::new_v4(),
+                name: "test2".to_string(),
+                price: 10,
+                timestamp: chrono::Utc::now(),
+                value: 1.0,
+                treated: false,
+            },
+        ];
+
+        let aggregated = _BillableController::_aggregate(billings);
+
+        assert_eq!(aggregated.len(), 2);
+        assert_eq!(aggregated[0].name, "test");
+        assert_eq!(aggregated[0].price, 20);
+        assert_eq!(aggregated[1].name, "test2");
+        assert_eq!(aggregated[1].price, 10);
     }
 }
