d3a013f13e9561494847c961fa0f12767fa0921d
WoodenMaiden <yann.pomie@laposte.net>
2023-06-01 18:57:27 +0200

    fix: aggregation by time and metrics works with hours
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

--- aggregator/src/bills/aggregators.rs
+++ aggregator/src/bills/aggregators.rs
@@ -1,4 +1,5 @@
 use common::models::billable::BillableSQL;
+use sqlx::types::chrono::{DateTime, Utc};
 
 use crate::bills::{AverageMetricPerHour, BillPerMetricAndHour};
 
@@ -19,7 +20,15 @@ fn aggregate_by_hour_and_metric(billings: Vec<BillableSQL>) -> Vec<BillPerMetric
         let bill_per_metric = BillPerMetricAndHour {
             id: None,
             name: bill.name,
-            hour: bill.timestamp,
+            // we get the hour by rounding down the timestamp
+            hour: DateTime::from_utc(
+                chrono::NaiveDateTime::from_timestamp_opt(
+                    bill.timestamp.timestamp() - bill.timestamp.timestamp() % 3600,
+                    0,
+                )
+                .unwrap_or(bill.timestamp.naive_utc()),
+                Utc,
+            ),
             price: bill.price,
         };
 
@@ -28,7 +37,7 @@ fn aggregate_by_hour_and_metric(billings: Vec<BillableSQL>) -> Vec<BillPerMetric
         } else {
             let mut found = false;
             for bill in bills_per_metric.iter_mut() {
-                if bill.name == bill_per_metric.name {
+                if bill.name == bill_per_metric.name && bill.hour == bill_per_metric.hour {
                     bill.price += bill_per_metric.price;
                     found = true;
                     break;
@@ -45,6 +54,8 @@ fn aggregate_by_hour_and_metric(billings: Vec<BillableSQL>) -> Vec<BillPerMetric
 
 #[cfg(test)]
 mod tests {
+    use chrono::TimeZone;
+
     use super::*;
 
     #[test]
@@ -54,7 +65,7 @@ mod tests {
                 id: 0,
                 name: "test".to_string(),
                 price: 10,
-                timestamp: chrono::Utc::now(),
+                timestamp: chrono::Utc.with_ymd_and_hms(2023, 3, 2, 10, 10, 9).unwrap(),
                 value: 1.0,
                 treated: false,
             },
@@ -62,7 +73,9 @@ mod tests {
                 id: 1,
                 name: "test".to_string(),
                 price: 10,
-                timestamp: chrono::Utc::now(),
+                timestamp: chrono::Utc
+                    .with_ymd_and_hms(2023, 3, 2, 10, 10, 36)
+                    .unwrap(),
                 value: 1.0,
                 treated: false,
             },
@@ -70,7 +83,19 @@ mod tests {
                 id: 2,
                 name: "test2".to_string(),
                 price: 10,
-                timestamp: chrono::Utc::now(),
+                timestamp: chrono::Utc
+                    .with_ymd_and_hms(2025, 3, 2, 10, 10, 36)
+                    .unwrap(),
+                value: 1.0,
+                treated: false,
+            },
+            BillableSQL {
+                id: 3,
+                name: "test2".to_string(),
+                price: 1234,
+                timestamp: chrono::Utc
+                    .with_ymd_and_hms(2025, 3, 2, 11, 10, 36)
+                    .unwrap(),
                 value: 1.0,
                 treated: false,
             },
@@ -78,10 +103,18 @@ mod tests {
 
         let aggregated = aggregate(billings);
 
-        assert_eq!(aggregated.0.len(), 2);
+        assert_eq!(aggregated.0.len(), 3);
         assert_eq!(aggregated.0[0].name, "test");
         assert_eq!(aggregated.0[0].price, 20);
         assert_eq!(aggregated.0[1].name, "test2");
         assert_eq!(aggregated.0[1].price, 10);
+        assert_eq!(aggregated.0[2].name, "test2");
+        assert_eq!(aggregated.0[2].price, 1234);
+
+        // we ensure that the hour is rounded down
+        assert_eq!(
+            aggregated.0[0].hour,
+            chrono::Utc.with_ymd_and_hms(2023, 3, 2, 10, 0, 0).unwrap()
+        );
     }
 }
