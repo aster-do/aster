43c27e3437b3a938f97d891fa283fda6d9a9c086
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2023-06-02 10:54:53 +0200

    fix: switch to async_channel
    Signed-off-by: Simon LUCIDO <simon.lucido@etu.umontpellier.fr>

--- common/src/messaging/crossbeam/mod.rs
+++ common/src/messaging/crossbeam/mod.rs
@@ -12,70 +12,72 @@ use thiserror::Error;
 use super::{AsyncReceiver, AsyncSender};
 
 #[derive(Error, Debug)]
-pub enum CrossbeamSenderError {
+pub enum AsyncChannelSenderError {
     #[error("Channel disconnected")]
     ChannelDisconnected,
 }
 
 #[derive(Error, Debug)]
-pub enum CrossbeamReceiverError {
+pub enum AsyncChannelReceiverError {
     #[error("ReceiveError {0}")]
-    ReceiveError(crossbeam_channel::RecvError),
+    ReceiveError(async_channel::RecvError),
 }
 
 #[derive(Clone)]
 pub struct CrossbeamReceiver<T: Send> {
-    receiver: crossbeam_channel::Receiver<T>,
+    receiver: async_channel::Receiver<T>,
 }
 
 #[derive(Clone)]
 pub struct CrossbeamSender<T: Send> {
-    sender: crossbeam_channel::Sender<T>,
+    sender: async_channel::Sender<T>,
 }
 
 impl<T: Send> CrossbeamReceiver<T> {
-    pub fn new(receiver: crossbeam_channel::Receiver<T>) -> Self {
+    pub fn new(receiver: async_channel::Receiver<T>) -> Self {
         Self { receiver }
     }
 }
 
 #[async_trait]
-impl<T: Send> AsyncReceiver<T, CrossbeamReceiverError> for CrossbeamReceiver<T> {
-    async fn receive(&mut self) -> Result<T, CrossbeamReceiverError> {
+impl<T: Send> AsyncReceiver<T, AsyncChannelReceiverError> for CrossbeamReceiver<T> {
+    async fn receive(&mut self) -> Result<T, AsyncChannelReceiverError> {
         self.receiver
             .recv()
-            .map_err(CrossbeamReceiverError::ReceiveError)
+            .await
+            .map_err(AsyncChannelReceiverError::ReceiveError)
     }
 }
 
 impl<T: Send> CrossbeamSender<T> {
-    pub fn new(sender: crossbeam_channel::Sender<T>) -> Self {
+    pub fn new(sender: async_channel::Sender<T>) -> Self {
         Self { sender }
     }
 }
 
 #[async_trait]
-impl<T: Send> AsyncSender<T, CrossbeamSenderError> for CrossbeamSender<T> {
-    async fn send(&mut self, message: T) -> Result<(), CrossbeamSenderError> {
+impl<T: Send> AsyncSender<T, AsyncChannelSenderError> for CrossbeamSender<T> {
+    async fn send(&mut self, message: T) -> Result<(), AsyncChannelSenderError> {
         self.sender
             .send(message)
-            .map_err(|_| CrossbeamSenderError::ChannelDisconnected)
+            .await
+            .map_err(|_| AsyncChannelSenderError::ChannelDisconnected)
     }
 }
 
 pub struct CrossbeamMessagingFactory {
-    billable_sender: crossbeam_channel::Sender<crate::models::Billable>,
-    billable_receiver: crossbeam_channel::Receiver<crate::models::Billable>,
-    metric_sender: crossbeam_channel::Sender<crate::models::Metric>,
-    metric_receiver: crossbeam_channel::Receiver<crate::models::Metric>,
+    billable_sender: async_channel::Sender<crate::models::Billable>,
+    billable_receiver: async_channel::Receiver<crate::models::Billable>,
+    metric_sender: async_channel::Sender<crate::models::Metric>,
+    metric_receiver: async_channel::Receiver<crate::models::Metric>,
 }
 
 impl CrossbeamMessagingFactory {}
 
 impl Default for CrossbeamMessagingFactory {
     fn default() -> Self {
-        let (billable_sender, billable_receiver) = crossbeam_channel::unbounded();
-        let (metric_sender, metric_receiver) = crossbeam_channel::unbounded();
+        let (billable_sender, billable_receiver) = async_channel::unbounded();
+        let (metric_sender, metric_receiver) = async_channel::unbounded();
 
         Self {
             billable_sender,
