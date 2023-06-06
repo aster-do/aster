ece982c7b8ba4648bd10f13044de08ac6f4b9800
Alexandre Gomez <gomez.a.corneille@gmail.com>
2023-06-06 14:44:29 +0200

    feat: add liveness server

--- aster/src/main.rs
+++ aster/src/main.rs
@@ -1,8 +1,17 @@
-use std::time::Duration;
+use std::{
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    time::Duration,
+};
 
+use axum::{routing::get, Router, Server};
 use billing::services::BillingService;
 use common::{services::AsterServiceError, AsterService};
-use log::info;
+use log::{error, info};
+use tokio::join;
+
+const LIVENESS_SERVER_ADDRESS: &SocketAddr =
+    &SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3029);
+const LIVENESS_SERVER_ENDPOINT: &str = "/health";
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -15,6 +24,9 @@ async fn main() -> Result<(), anyhow::Error> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     info!("Starting Aster");
+    let liveness_app = Router::new().route(LIVENESS_SERVER_ENDPOINT, get(liveness_handler));
+    let liveness_server =
+        Server::bind(LIVENESS_SERVER_ADDRESS).serve(liveness_app.into_make_service());
 
     let mut _messager = common::messaging::tokio_broadcast::CrossbeamMessagingFactory::default();
 
@@ -47,7 +59,24 @@ async fn main() -> Result<(), anyhow::Error> {
         handles.push(service.run());
     }
 
-    futures::future::join_all(handles).await;
+    info!("Running liveness server");
+    let (services_result, liveness_result) =
+        join!(futures::future::join_all(handles), liveness_server);
+
+    // Handle post execution errors
+    services_result.iter().for_each(|result| {
+        if let Err(e) = result {
+            error!("Service failed: {}", e);
+        }
+    });
+
+    if let Err(e) = liveness_result {
+        error!("Liveness server failed: {}", e);
+    }
 
     Ok(())
+}
+
+async fn liveness_handler() -> &'static str {
+    "OK"
 }
