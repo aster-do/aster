35ab679d8ad2abe520fbe868b14abd9c8c53aa64
Alexis-Bernard <alexisbernard34310@gmail.com>
2023-06-06 18:45:15 +0200

    feat(controller): add CRUD queries

--- common/src/models/billable_rules/billable_rule_persistent.rs
+++ common/src/models/billable_rules/billable_rule_persistent.rs
@@ -1,3 +1,7 @@
+use anyhow::Ok;
+
+use super::billable_rule::{BillableOperation, BillableRule};
+
 /// BillableRule is a struct that represents a rule for how to bill a customer
 /// for each operation. It contains a name, operation, and number.
 /// Operation is an enum that can be Add, Subtract, Multiply, or Divide.
@@ -13,6 +17,24 @@ pub struct BillableRulePersistent {
     pub version: i32,
 }
 
+impl BillableRulePersistent {
+    pub fn from(rule: &BillableRule) -> Result<Self, anyhow::Error> {
+        let billable_rule_persistent = Self {
+            id: rule
+                .id
+                .ok_or_else(|| anyhow::anyhow!("BillableRulePersistent::from: id is None"))?,
+            name: rule.name.clone(),
+            operation: BillableOperationPersistent::from(&rule.operation),
+            number: rule.number as i32,
+            version: rule
+                .version
+                .ok_or_else(|| anyhow::anyhow!("BillableRulePersistent::from: version is None"))?,
+        };
+
+        Ok(billable_rule_persistent)
+    }
+}
+
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, sqlx::Type)]
 #[sqlx(type_name = "billable_operation")]
 pub enum BillableOperationPersistent {
@@ -20,4 +42,15 @@ pub enum BillableOperationPersistent {
     Subtract,
     Multiply,
     Divide,
+}
+
+impl BillableOperationPersistent {
+    pub fn from(operation: &BillableOperation) -> Self {
+        match operation {
+            BillableOperation::Add => Self::Add,
+            BillableOperation::Subtract => Self::Subtract,
+            BillableOperation::Multiply => Self::Multiply,
+            BillableOperation::Divide => Self::Divide,
+        }
+    }
 }
