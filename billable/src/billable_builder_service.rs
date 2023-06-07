1a6eb318134b9268c70fbadbd7ab03630d0a81f6
iverly <github@iverly.net>
2023-06-07 15:06:50 +0200

    feat: listen billable rules from billable
    Signed-off-by: iverly <github@iverly.net>

--- billable/src/billable_builder_service.rs
+++ billable/src/billable_builder_service.rs
@@ -1,16 +1,18 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::sync::Arc;
 
 use crate::transformer::transform;
 use anyhow::anyhow;
 use async_trait::async_trait;
 use axum::{routing::get, Router, Server};
 use common::messaging::tokio_broadcast::{
-    BillableSender, CrossbeamMessagingFactory, MetricReceiver,
+    BillableRuleReceiver, BillableSender, CrossbeamMessagingFactory, MetricReceiver,
 };
 use common::messaging::{AsyncReceiver, AsyncSender, MessagingFactory};
-use common::models::billable_rules::billable_rule::{BillableOperation, BillableRule};
+use common::models::billable_rules::billable_rule::BillableRule;
 use common::monitoring::readiness_handler;
 use common::AsterService;
+use tokio::sync::RwLock;
 
 const READINESS_SERVER_ADDRESS: &SocketAddr =
     &SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3038);
@@ -18,9 +20,10 @@ const READINESS_SERVER_ENDPOINT: &str = "/health";
 
 #[derive(Default)]
 pub struct BillableBuilderService {
-    pub rules: Vec<BillableRule>,
+    pub rules: Arc<RwLock<Vec<BillableRule>>>,
     pub metric_receiver: Option<MetricReceiver>,
     pub billable_sender: Option<BillableSender>,
+    pub billable_rule_receiver: Option<BillableRuleReceiver>,
 }
 
 #[async_trait]
@@ -31,23 +34,10 @@ impl AsterService for BillableBuilderService {
     ) -> Result<(), anyhow::Error> {
         self.metric_receiver = Some(messaging.create_metric_receiver().await);
         self.billable_sender = Some(messaging.create_billable_sender().await);
+        self.billable_rule_receiver = Some(messaging.create_billable_rule_receiver().await);
 
-        self.rules = vec![
-            BillableRule {
-                id: Some(1),
-                name: "cpu".to_string(),
-                operation: BillableOperation::Multiply,
-                number: 1,
-                version: Some(1),
-            },
-            BillableRule {
-                id: Some(2),
-                name: "memory".to_string(),
-                operation: BillableOperation::Multiply,
-                number: 3,
-                version: Some(1),
-            },
-        ];
+        // init billable rules state
+        self.rules = Arc::new(RwLock::new(vec![]));
 
         log::info!("BillableBuilderService initialized");
         Ok(())
@@ -60,17 +50,34 @@ impl AsterService for BillableBuilderService {
         let readiness_server =
             Server::bind(READINESS_SERVER_ADDRESS).serve(readiness_app.into_make_service());
 
-        let (readiness_result, lifecycle_result) = tokio::join!(readiness_server, self.lifecycle());
+        let (
+            readiness_result,
+            listen_metrics_result,
+            listen_billable_rules_result,
+            fetch_billable_rules_result,
+        ) = tokio::join!(
+            readiness_server,
+            self.listen_metrics(),
+            self.listen_billable_rules(),
+            self.fetch_billable_rules()
+        );
+
         readiness_result.map_err(|e| anyhow!("Readiness server failed").context(e))?;
-        lifecycle_result
-            .map_err(|e| anyhow!("BillableBuilderService lifecycle failed").context(e))?;
+        listen_billable_rules_result.map_err(|e| {
+            anyhow!("BillableBuilderService listen_billable_rules failed").context(e)
+        })?;
+        listen_metrics_result
+            .map_err(|e| anyhow!("BillableBuilderService listen_metrics failed").context(e))?;
+        fetch_billable_rules_result.map_err(|e| {
+            anyhow!("BillableBuilderService fetch_billable_rules failed").context(e)
+        })?;
 
         Ok(())
     }
 }
 
 impl BillableBuilderService {
-    async fn lifecycle(&self) -> Result<(), anyhow::Error> {
+    async fn listen_metrics(&self) -> Result<(), anyhow::Error> {
         let metric_receiver = self.metric_receiver.clone().ok_or_else(|| {
             anyhow::anyhow!("Metric receiver not initialized. Did you forget to call init()?")
         })?;
@@ -91,7 +98,7 @@ impl BillableBuilderService {
             };
 
             log::debug!("Received metric: {:?}", metric);
-            let billable = transform(&metric, self.rules.clone());
+            let billable = transform(&metric, self.rules.clone()).await;
             log::debug!("Sending billable: {:?}", billable);
 
             match billable_sender.clone().send(billable).await {
@@ -101,5 +108,35 @@ impl BillableBuilderService {
 
             log::debug!("Billable sent");
         }
+    }
+
+    async fn listen_billable_rules(&self) -> Result<(), anyhow::Error> {
+        let billable_rule_receiver = self.billable_rule_receiver.clone().ok_or_else(|| {
+            anyhow::anyhow!(
+                "Billable rule receiver not initialized. Did you forget to call init()?"
+            )
+        })?;
+
+        // In case of an error, we choose to simply log the error and continue.
+        loop {
+            let received_billable_rule = billable_rule_receiver.clone().receive().await;
+            let billable_rule = match received_billable_rule {
+                Ok(billable_rule) => billable_rule,
+                Err(e) => {
+                    log::error!("Error receiving billable rule: {:?}", e);
+                    continue;
+                }
+            };
+
+            log::debug!("Received billable rule: {:?}", billable_rule);
+
+            // check if rule already exists
+            // todo: deduplication
+            self.rules.write().await.push(billable_rule);
+        }
+    }
+
+    async fn fetch_billable_rules(&self) -> Result<(), anyhow::Error> {
+        todo!()
     }
 }
