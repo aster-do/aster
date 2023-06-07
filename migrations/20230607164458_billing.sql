028392c89d86503445c1b1588149fc56d6316257
iverly <github@iverly.net>
2023-06-07 19:10:38 +0200

    fix: migration billing script
    Signed-off-by: iverly <github@iverly.net>

--- migrations/20230607164458_billing.sql
+++ migrations/20230607164458_billing.sql
@@ -1,7 +1,7 @@
 -- Table Definition ----------------------------------------------
 
 CREATE TABLE billing (
-    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id integer PRIMARY KEY,
     generated_at timestamp with time zone NOT NULL,
     items text
 );
