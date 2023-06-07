4051b72e32c6e4a29defe5ddce56d05b4beb6f7a
RedbeanGit <dubois.julien.mail@gmail.com>
2023-06-07 15:04:34 +0200

    feat: integrate new components
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
   ],
   rules: {
     "@typescript-eslint/explicit-function-return-type": "off",
+    "react/require-default-props": "off",
   },
   ignorePatterns: [
     ".eslintrc.js"
